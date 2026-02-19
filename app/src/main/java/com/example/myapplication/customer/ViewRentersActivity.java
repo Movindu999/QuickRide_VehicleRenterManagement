271b615643a4ffa1d33f9ebe77a914eba0bc4a2f
Movindu999 <user@example.com>
2026-02-19 10:52:49 +0530

    Add Customer UI screens and navigation

---
+++ app/src/main/java/com/example/myapplication/customer/ViewRentersActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.customer;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.myapplication.R;
+
+public class ViewRentersActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_renters);
+    }
+}
