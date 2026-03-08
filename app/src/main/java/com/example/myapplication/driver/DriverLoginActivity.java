d714f001bda4a09e1bdfac8836cf1a07d71691a1
dilki999 <dilkichamoda50@gmail.com>
2026-03-08 11:09:00 +0530

    Driver login UI added

---
+++ app/src/main/java/com/example/myapplication/driver/DriverLoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication.driver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class DriverLoginActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(v ->
+                startActivity(new Intent(DriverLoginActivity.this, DriverDashboardActivity.class)));
+    }
+}
